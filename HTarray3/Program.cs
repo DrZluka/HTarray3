9c5e5a020b4b3bef59ae2ec528f33442d89a7093
Федор-ПК\Федор <curafedor@gmail.com>
2021-01-09 14:03:38 +0200

    HTarray3

---
+++ HTarray3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HTarray3
+{
+    class Program
+    {
+        static void AtherSide(ref int[] array)
+        {
+            for (int i = 1; i <= array.Length; i++)
+            {
+                Console.Write(array[^i] + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 4, 5, 6, 7, 8, 9 };
+
+            AtherSide(ref myArray);
+        }
+    }
+}
